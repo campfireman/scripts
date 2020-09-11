78ab1c21ddfc596c41c6fb3647e06f48c4cee153
Ture <ture.cl.business@gmail.com>
2020-09-11 15:59:47 +0200

    comments

--- init_git.sh
+++ init_git.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
 
 REPOSITORY_NAME=${PWD##*/}
-
 FULL_INIT=0
 DEFAULT_FILES=".gitignore"
 VISIBILITY="p"
-REMOTE_VAR="origin"
-GITLAB_URL="https://gitlab.com"
-TOKEN="_zM-YLzDvHXNezGYcSZy"
 
 # Loop through arguments and process them
 for arg in "$@"
@@ -34,13 +30,16 @@ if [ ${FULL_INIT} -eq 1 ]
 then
 	mkdir ${REPOSITORY_NAME} && cd ${REPOSITORY_NAME} && git init && touch ${DEFAULT_FILES} && cd ..;
 else
+    # glab can't be inside of the git repository
     cd ..;
 fi
+
 echo "$REPOSITORY_NAME"
 URL=$(glab repo create "${REPOSITORY_NAME}" -${VISIBILITY})
 searchstring="Project created:  https://"
 echo "Repository created!"
 
+# convert to ssh url (replace the first slash)
 URL=git@"${URL#*$searchstring}".git
 URL=$(echo "$URL" | sed '0,/\//s//:/')
 

7c366a8c5e67c3c70d620b3c364a8a6f799d550b
Ture Claussen <ture.cl.business@gmail.com>
2021-10-30 22:18:32 +0200

    Add script for installing arch packages

---
+++ install_arch_packages.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+import json
+import subprocess
+
+with open('pkg_list.json', 'r') as file:
+    pkg_list = json.loads(file.read())
+
+    # install standard packages
+    subprocess.call(['sudo', 'pacman', '--noconfirm',
+                    '-S', *pkg_list['standard']])
+
+    # install AUR packages
+    subprocess.call(['yay', '--sudoloop',  '--answerclean',
+                    'NotInstalled', '--answerdiff', 'None', '-S', *pkg_list['aur']])

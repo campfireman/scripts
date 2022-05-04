8ec3a1009c80a1b5a28b97591ba35a599d469104
= <=>
2022-05-04 18:46:39 +0200

    Added display setup script

---
+++ setup_displays.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import os
+import subprocess
+from argparse import ArgumentParser
+
+
+def main():
+    script_dir = os.path.dirname(__file__)
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        'device_name',
+        type=str,
+        help='The device name in the settings file',
+    )
+    parser.add_argument(
+        'setup_name',
+        type=str,
+        help='The name of the setup for a given device',
+    )
+    args = parser.parse_args()
+
+    with open(os.path.join(script_dir, "assets", "display_settings.json")) as settings_file:
+        settings = json.loads(settings_file.read())
+
+    try:
+        setup = settings[args.device_name][args.setup_name]
+    except KeyError:
+        raise Exception(
+            f'Setup not found for device {args.device_name} and setup {args.setup_name}!')
+
+    for display, display_command in setup.items():
+        command = f'xrandr --output {display} {display_command}'
+        print(f'Running command: {command}')
+        exit_code = subprocess.call(command.split(' '))
+        if exit_code != 0:
+            raise Exception(f'Command {command} return non 0 exit code!')
+
+
+if __name__ == '__main__':
+    main()

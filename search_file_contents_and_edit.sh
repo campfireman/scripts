354f573c2f1a67f6ed0a8b0b5a2240889d2c03c0
Ture Claussen <ture.cl.business@gmail.com>
2022-05-29 11:53:20 +0200

    Added script for searching and editing file contents

---
+++ search_file_contents_and_edit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# arguments
+PATTERN=$1
+shift
+DIRECTORIES=${@}
+
+function print_usage() {
+cat << EOF
+
+SYNOPSIS:
+	Usage: ${0} PATTERN [DIRECTORY ...]
+
+DESCRIPTION:
+	Recurses into given directories and searches for a given pattern
+	within those files. Selected file and line are then opened with
+	default editor.
+
+ARGUMENTS:
+	PATTERN is used by grep with argument -i, thus case is ignored
+	If DIRECTORY is not set the current path is selected as search path
+	
+EOF
+}
+
+if [ -z "${PATTERN}" ]; then
+	print_usage
+	exit 1
+fi
+
+echo ${DIRECTORIES}
+if [ -z "${DIRECTORIES}" ]; then
+	DIRECTORIES="./"
+fi
+
+SELECTED_FILE=$(grep -irn "${PATTERN}" ${DIRECTORIES} 2> /dev/null | sk)
+SELECTED_FILE_PATH=$(echo "${SELECTED_FILE}" | grep -Po '^[^:]*')
+SELECTED_FILE_LINE=$(echo "${SELECTED_FILE}" | grep -Po '(?<=:)[[:digit:]]+(?=:)')
+
+if [ -z "${SELECTED_FILE_PATH}" ]; then
+	echo "No matches."
+	exit 0
+fi
+
+${EDITOR} "${SELECTED_FILE_PATH}" -c ":${SELECTED_FILE_LINE}"

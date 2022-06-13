7708eff0a653c419593e0b6f7c62bc528878d582
Ture Claussen <ture.claussen@otto.de>
2022-06-13 15:30:20 +0200

    Added chromium installer

---
+++ assets/installers/install_chromium.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# installs chromium from system76 PPA: https://www.linuxadictos.com/en/how-to-install-chromium-on-ubuntu-20-04-without-using-its-snap-package.html
+sudo add-apt-repository ppa:system76/pop
+sudo apt update && sudo apt install chromium
+
